e0d8a55f6b34ebc280dbebd041372bf24aa7da33
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2024-06-08 12:03:04 -0300

    wip: line partitioner

--- src/partitioning/line_partitioner.go
+++ src/partitioning/line_partitioner.go
@@ -3,7 +3,9 @@ package partitioning
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
+	"math"
 	"os"
 	"path"
 )
@@ -14,55 +16,91 @@ func NewLinePartitioner() LinePartitioner {
 	return LinePartitioner{}
 }
 
-func (partitioner *LinePartitioner) Partition(filepath string, maxNumberOfPartitions uint32) ([]string, error) {
+func (partitioner *LinePartitioner) Partition(filepath string, maxNumberOfPartitions uint32) (partitionFilePaths []string, err error) {
 	file, err := os.Open(filepath)
 	if err != nil {
 		return nil, fmt.Errorf("opening input file: filepath=%s %w", filepath, err)
 	}
+	defer func() {
+		if closeErr := file.Close(); closeErr != nil {
+			err = errors.Join(err, fmt.Errorf("closing input file: filepath=%s %w", filepath, err))
+		}
+	}()
 	fileInfo, err := file.Stat()
 	if err != nil {
 		return nil, fmt.Errorf("getting file stats: %w", err)
 	}
 
-	partitionSize := fileInfo.Size() / int64(maxNumberOfPartitions)
-	partitionFilePaths := make([]string, 0, maxNumberOfPartitions)
+	// partition per lines per file
+	// create n partition files
+	// write lines to files in round robin order
+
+	partitionSizeInBytes := int64(math.Ceil(float64(fileInfo.Size()+int64(maxNumberOfPartitions)) / float64(maxNumberOfPartitions)))
+	partitionFilePaths = make([]string, 0, maxNumberOfPartitions)
 
 	scanner := bufio.NewScanner(file)
-	scanner.Buffer(make([]byte, partitionSize), int(partitionSize))
+	scanner.Buffer(make([]byte, partitionSizeInBytes), int(partitionSizeInBytes))
 
 	directory, _ := path.Split(filepath)
 
-	for i := 0; i < int(maxNumberOfPartitions); i++ {
-		partitionBuffer := bytes.NewBuffer(make([]byte, 0, partitionSize))
+	partitionBuffer := bytes.NewBuffer(make([]byte, 0, partitionSizeInBytes))
+	partitionNumber := 0
 
-		for scanner.Scan() {
-			line := scanner.Text()
+	for scanner.Scan() {
+		line := scanner.Text()
 
-			if partitionBuffer.Len()+len(line) > int(partitionSize) {
-				// TOOD: the last scanned `line` is being lost
-				break
+		// Partition buffer is full, write to disk.
+		// + 1 for the new line that will be added at the end.
+		if partitionBuffer.Len()+len(line)+1 > int(partitionSizeInBytes) && partitionNumber < int(maxNumberOfPartitions) {
+			partitionFilePath, err := writePartitionFile(directory, partitionNumber, partitionBuffer.Bytes())
+			if err != nil {
+				return partitionFilePaths, fmt.Errorf("writing partition file to disk: %w", err)
 			}
-			if partitionBuffer.Len()+len(line) <= int(partitionSize) {
-				_, err := partitionBuffer.WriteString(line)
-				if err != nil {
-					return nil, fmt.Errorf("writing to parititon buffer: %w", err)
-				}
-			}
+			partitionBuffer.Reset()
+			partitionFilePaths = append(partitionFilePaths, partitionFilePath)
+			partitionNumber++
 		}
 
-		if partitionBuffer.Len() > 0 {
-			partitionFilePath := fmt.Sprintf("%s_input_%d", directory, i)
-			partitionFilePaths = append(partitionFilePaths, partitionFilePath)
-			partitionFile, err := os.OpenFile(partitionFilePath, os.O_RDWR|os.O_CREATE, 0644)
-			if err != nil {
-				return nil, fmt.Errorf("creating/opening partition file: filepath=%s %w", partitionFilePath, err)
-			}
-			bytesWritten, err := partitionFile.Write(partitionBuffer.Bytes())
-			if err != nil {
-				return nil, fmt.Errorf("writing to input partition file: bytesWritten=%d %w", bytesWritten, err)
-			}
+		if _, err := partitionBuffer.WriteString(line); err != nil {
+			return nil, fmt.Errorf("writing to parititon buffer: %w", err)
+		}
+		if err := partitionBuffer.WriteByte('\n'); err != nil {
+			return nil, fmt.Errorf("writing new line to partition buffer: %w", err)
 		}
 	}
 
+	// There's some data in the partition buffer, write to disk.
+	if partitionBuffer.Len() > 0 {
+		partitionFilePath, err := writePartitionFile(directory, partitionNumber, partitionBuffer.Bytes())
+		if err != nil {
+			return partitionFilePaths, fmt.Errorf("writing partition file to disk: %w", err)
+		}
+		partitionBuffer.Reset()
+		partitionFilePaths = append(partitionFilePaths, partitionFilePath)
+	}
+
 	return partitionFilePaths, nil
+}
+
+func writePartitionFile(directory string, partitionNumber int, buffer []byte) (partitionFilePath string, err error) {
+	partitionFilePath = fmt.Sprintf("%s_input_%d", directory, partitionNumber)
+
+	partitionFile, err := os.OpenFile(partitionFilePath, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return partitionFilePath, fmt.Errorf("creating/opening partition file: filepath=%s %w", partitionFilePath, err)
+	}
+	defer func() {
+		if closeErr := partitionFile.Close(); closeErr != nil {
+			err = errors.Join(err, fmt.Errorf("closing partiiton file: %w", closeErr))
+		}
+	}()
+	bytesWritten, err := partitionFile.Write(buffer)
+	if err != nil {
+		return partitionFilePath, fmt.Errorf("writing to input partition file: bytesWritten=%d %w", bytesWritten, err)
+	}
+	if err := partitionFile.Sync(); err != nil {
+		return partitionFilePath, fmt.Errorf("syncing partition file: filepath=%s %w", partitionFilePath, err)
+	}
+
+	return partitionFilePath, nil
 }
--- src/partitioning/line_partitioner_test.go
+++ src/partitioning/line_partitioner_test.go
@@ -9,15 +9,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func createTempFile(fileName string) (*os.File, error) {
+	directory, err := os.MkdirTemp("", "line_partitioner_*")
+	if err != nil {
+		return nil, fmt.Errorf("creating temporary directory: %w", err)
+	}
+	file, err := os.CreateTemp(directory, fileName)
+	if err != nil {
+		return file, fmt.Errorf("creating temporary file: %w", err)
+	}
+	return file, nil
+}
+
 func TestPartition(t *testing.T) {
 	t.Parallel()
 
-	file, err := os.CreateTemp("", "line_partitioner")
+	file, err := createTempFile("line_partitioner")
 	assert.NoError(t, err)
 
-	for i := 0; i < 10; i++ {
+	numberOfFileLines := 10
+	maxNumberOfPartitions := 3
 
-		_, err := file.WriteString(fmt.Sprintf("partition %d\n", i))
+	for i := 0; i < int(numberOfFileLines); i++ {
+		_, err := file.WriteString(fmt.Sprintf("line_%d\n", i))
 		assert.NoError(t, err)
 	}
 
@@ -26,17 +40,19 @@ func TestPartition(t *testing.T) {
 	assert.NoError(t, err)
 
 	partitioner := NewLinePartitioner()
-	partitionFilePaths, err := partitioner.Partition(file.Name(), 10)
+	partitionFilePaths, err := partitioner.Partition(file.Name(), uint32(maxNumberOfPartitions))
 	assert.NoError(t, err)
 	fmt.Printf("\n\naaaaaaa partitionFilePaths %+v\n\n", partitionFilePaths)
 
-	for i, filepath := range partitionFilePaths {
+	for _, filepath := range partitionFilePaths {
+
 		file, err := os.Open(filepath)
 		assert.NoError(t, err)
 
 		contents, err := io.ReadAll(file)
 		assert.NoError(t, err)
 
-		assert.Equal(t, fmt.Sprintf("partition %d", i), string(contents))
+		fmt.Printf("\n\naaaaaaa string(contents)\n%+v\n\n", string(contents))
+
 	}
 }
